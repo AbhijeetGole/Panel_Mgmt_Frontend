eaa6e3cb5146bc4fab69ef0d4a899b44e9a480a6
vivek-1-2-3 <78726663+vivek-1-2-3@users.noreply.github.com>
2022-09-21 12:01:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { useState } from 'react';
+
 
 //import pages
 import Createpanel from './Components/Createpanel.js';
@@ -11,8 +13,10 @@ import Updatepanel from './Components/Updatepanel.js';
 import Updateslot from './Components/Updateslot.js';
 import Userlogin from './Components/Userlogin.js';
 import Userregister from './Components/Userregister.js';
-import Dash from './Components/Dash.js'
-import Dash2 from './Components/Dash2.js'
+import Dash from './Components/Dash.js';
+import Dash2 from './Components/Dash2.js';
+import Calend from './Components/Calendar.tsx';
+
 
 
 
@@ -32,6 +36,7 @@ const App = () => {
         <Route path="/Updateslot" element={<Updateslot/>} />
         <Route path="/Dashboard" element={<Dash/>} />
         <Route path="/Dashboard2" element={<Dash2/>} />
+        <Route path="/Calendar" element={<Calend/>} />
       </Routes>  
     </Router>
   );
