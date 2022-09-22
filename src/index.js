136577b721410e128cb8b1221e676658e601bf62
vivek-1-2-3 <78726663+vivek-1-2-3@users.noreply.github.com>
2022-09-22 14:30:34 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,22 +4,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 //import reportWebVitals from './reportWebVitals';
-// import {BrowserRouter as Router} from "react-router-dom";
-// import {Route,Switch,Routes} from "react-router-dom";
 
 
 
-//import pages
- /* import Createpanel from './Pages/panel-slot/create-panel/Createpanel.js';
-  import Createslot from './Pages/panel-slot/create-slot/Createslot.js';
-  import Getallpanels from './Pages/panel-slot/get-all-panels/Getallpanels.js';
-  import Getallslots from './Pages/panel-slot/get-all-slots/Getallslots.js';
-  import Updatepanel from './Pages/panel-slot/update-panel/Updatepanel.js';
-  import Updateslot from './Pages/panel-slot/update-slot/Updateslot.js';
-  import Userlogin from './Pages/user/user-login/Userlogin.js';
-  import Userregister from './Pages/user/user-register/Userregister.js';*/
- 
-//
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
