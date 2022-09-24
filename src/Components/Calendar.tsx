4f33c716c598bcc58df22176248d2dfd5a3dee4d
vivek-1-2-3 <78726663+vivek-1-2-3@users.noreply.github.com>
2022-09-24 14:59:35 +0530

    Add files via upload

---
+++ src/Components/Calendar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Assets/calendar.module.css";
+import { ScheduleComponent, Inject, Agenda, Day, Month, Week, WorkWeek, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { DataManager,WebApiAdaptor } from '@syncfusion/ej2-data';
+class Calend extends React.Component 
+{
+  private localData: EventSettingsModel = {
+    dataSource: [{
+      EndTime: new Date(2019, 0, 11, 6, 30),
+      StartTime: new Date(2019, 0, 11, 4, 0)
+    }]
+  };
+  private remoteData = new DataManager({
+    url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData', 
+    adaptor: new WebApiAdaptor(), 
+    crossDomain: true 
+  });
+ 
+  
+  render() 
+  {
+    return (
+    <div style= {{'marginLeft':'16.7%'}} >  
+      <ScheduleComponent currentView='Month'
+      eventSettings={{ dataSource: this.remoteData }} selectedDate={new Date(2017, 5, 5)} > 
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent> 
+    </div>   
+      
+    );
+  }
+}
+   
+
+export default Calend;
