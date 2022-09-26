d89521242a3b8a2863367b3dd69d15bb7854fab0
Yash Wakte <71642220+yashwakte@users.noreply.github.com>
2022-09-26 09:49:40 +0530

    Created an auth service file and services folder

---
+++ services/auth-service.js
@@ -0,0 +1,91 @@
+import axios from 'axios'
+const API_URL= "http://localhost:8080"
+const API_URL1= "http://localhost:4000/panel"
+const login=async(username,password)=>{
+    // return axios
+    //   .post(API_URL + "/login", {
+    //     username,
+    //     password
+    //   })
+    //   .then(response => {
+    //     if (response.data.accessToken) {
+    //       localStorage.setItem("user", JSON.stringify(response.data));
+    //     }
+    //     console.log(response.data)
+        const user={
+          username,
+          password
+        }
+        const response =await axios.post(API_URL+'/user/login',user);
+        return response.data;
+      
+}
+
+const register=async(firstname, lastname,username,password, email, phone)=>{
+    // return axios
+    //   .post(API_URL + "/login", {
+    //     username,
+    //     password
+    //   })
+    //   .then(response => {
+    //     if (response.data.accessToken) {
+    //       localStorage.setItem("user", JSON.stringify(response.data));
+    //     }
+    //     console.log(response.data)
+        const user={
+            firstname,
+            lastname,
+          username,
+          password,
+          email,
+          phone
+        }
+        const response =await axios.post(API_URL+'/user/register',user);
+        return response.data;    
+}
+
+const createSlot=async(slotId, panelName, startDate,endDate, starttime, endtime, skills, slotused, active) =>
+{
+    const slot={
+        slotId, panelName, startDate,
+        endDate, 
+        starttime, 
+        endtime, 
+        skills,
+        slotused,
+        active
+    }
+    console.log("In slot api")
+    const response =await axios.post(API_URL+'/slot/createSlot',slot);
+    console.log(response);
+    return response.data; 
+}
+
+const createPanel = async(panelId,panelName,skills,Interviewer)=>{
+    const Active=true;
+    const panel={
+      panelId,
+      panelName,
+      skills,
+      Interviewer,
+      Active
+    }
+    console.log("In Create Panel Api");
+    const response1 =await axios.post (API_URL1+'/createPanel',panel);
+    return response1.data;
+
+}
+
+const getSlot = async()=>{
+    const response1 =await axios.get (API_URL+'/slot/getSlot');
+    return response1.data;
+}
+
+const Authservice={
+    login,
+    register,
+    createSlot,
+    createPanel,
+    getSlot
+}
+export default Authservice
