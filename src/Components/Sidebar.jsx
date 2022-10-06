874e7fc8f556fdd70a2abd1fe209451ef711ee08
vivek-1-2-3 <78726663+vivek-1-2-3@users.noreply.github.com>
2022-10-06 11:39:36 +0530

    Update Sidebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,6 +4,14 @@ import "./Assets/createslot.module.css";
 //class Sidebar extends React.Component 
 const Sidebar=()=>
 {
+  const paths = window.location.pathname;
+
+  if(paths=='/Userregister'||paths=='/')
+  {
+      return null; 
+  }
+  else
+  {  
 
       return (
      <div>
@@ -37,14 +45,14 @@ const Sidebar=()=>
                       </Link> 
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" Link='/'>
+                      <Link to="/Profile"className="nav-link" >  
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                       </svg>&nbsp;&nbsp;
                         <span data-feather="users" className="align-text-bottom" />
 
                         Profile
-                      </a>
+                      </Link> 
                     </li>
                     <li className="nav-item">
                       <Link to="/Calendar" className="nav-link">
@@ -100,5 +108,5 @@ const Sidebar=()=>
     );
     
   }
-
+}
 export default Sidebar;
